b26701fce88ab5716c494576db1dcfa6a1c1e155
László Miklós Vajay <vajaylm@gmail.com>
2023-01-24 11:49:39 +0100

    Add MQTT handling, publish sensor data to topic

--- NodeMCU/main.cpp
+++ NodeMCU/main.cpp
@@ -4,7 +4,11 @@
 #include <Adafruit_BME280.h>
 #include <ESP8266WiFi.h>
 #include <Ticker.h>
+#include <AsyncMqttClient.h>
 #include "defines.h"
+
+AsyncMqttClient mqttClient;
+Ticker mqttReconnectTimer;
 
 WiFiEventHandler wifiConnectHandler;
 WiFiEventHandler wifiDisconnectHandler;
@@ -24,15 +28,47 @@ void connectToWifi()
     WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 }
 
+void connectToMqtt()
+{
+    Serial.println("Connecting to MQTT...");
+    mqttClient.connect();
+}
+
 void onWifiConnect(const WiFiEventStationModeGotIP &event)
 {
     Serial.println("Connected to Wi-Fi.");
+    connectToMqtt();
 }
 
 void onWifiDisconnect(const WiFiEventStationModeDisconnected &event)
 {
     Serial.println("Disconnected from Wi-Fi.");
+    mqttReconnectTimer.detach(); // ensure we don't reconnect to MQTT while reconnecting to Wi-Fi
     wifiReconnectTimer.once(2, connectToWifi);
+}
+
+void onMqttConnect(bool sessionPresent)
+{
+    Serial.println("Connected to MQTT.");
+    Serial.print("Session present: ");
+    Serial.println(sessionPresent);
+}
+
+void onMqttDisconnect(AsyncMqttClientDisconnectReason reason)
+{
+    Serial.println("Disconnected from MQTT.");
+
+    if (WiFi.isConnected())
+    {
+        mqttReconnectTimer.once(2, connectToMqtt);
+    }
+}
+
+void onMqttPublish(uint16_t packetId)
+{
+    Serial.print("Publish acknowledged.");
+    Serial.print("  packetId: ");
+    Serial.println(packetId);
 }
 
 void setup()
@@ -45,12 +81,16 @@ void setup()
         Serial.println("Could not find a valid BME280 sensor, check wiring!");
         ESP.restart();
     }
-
     bme.setTemperatureCompensation(-0.9);
 
     wifiConnectHandler = WiFi.onStationModeGotIP(onWifiConnect);
     wifiDisconnectHandler = WiFi.onStationModeDisconnected(onWifiDisconnect);
 
+    mqttClient.onConnect(onMqttConnect);
+    mqttClient.onDisconnect(onMqttDisconnect);
+    mqttClient.onPublish(onMqttPublish);
+    mqttClient.setServer(MQTT_HOST, MQTT_PORT);
+    mqttClient.setCredentials(MQTT_USER, MQTT_PASSWORD);
     connectToWifi();
 }
 
@@ -60,15 +100,17 @@ void loop()
     if (currentMillis - previousPublish >= interval)
     {
         previousPublish = currentMillis;
+        
+        // BME280 sensor readings
         temperature = bme.readTemperature();
         humidity = bme.readHumidity();
         pressure = bme.readPressure() / 100.0F;
 
-        Serial.print("Temperature: ");
-        Serial.println(temperature);
-        Serial.print("Humidity: ");
-        Serial.println(humidity);
-        Serial.print("Pressure: ");
-        Serial.println(pressure);
-    }
+        String publishData = "{\"temperature\": " + String(temperature) + ", \"humidity\": " + String(humidity) + ", \"pressure\": " + String(pressure) + "}";
+
+        // Publish an MQTT message
+        Serial.println("Message: " + publishData);
+        int packetIdPub = mqttClient.publish(MQTT_PUB_SENSOR, 1, true, publishData.c_str());
+        Serial.printf("Publishing on topic %s at QoS 1, packetId: %i ", MQTT_PUB_SENSOR, packetIdPub);
+    }    
 }
