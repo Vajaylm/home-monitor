d2e6a82e07f844016077869d66939492f2573501
László Miklós Vajay <vajaylm@gmail.com>
2023-01-24 11:38:25 +0100

    Add Wifi handling

--- NodeMCU/main.cpp
+++ NodeMCU/main.cpp
@@ -2,11 +2,35 @@
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
+#include <ESP8266WiFi.h>
+#include <Ticker.h>
+#include "defines.h"
+
+WiFiEventHandler wifiConnectHandler;
+WiFiEventHandler wifiDisconnectHandler;
+Ticker wifiReconnectTimer;
 
 Adafruit_BME280 bme;
 float temperature;
 float humidity;
 float pressure;
+
+void connectToWifi()
+{
+    Serial.println("Connecting to Wi-Fi...");
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+}
+
+void onWifiConnect(const WiFiEventStationModeGotIP &event)
+{
+    Serial.println("Connected to Wi-Fi.");
+}
+
+void onWifiDisconnect(const WiFiEventStationModeDisconnected &event)
+{
+    Serial.println("Disconnected from Wi-Fi.");
+    wifiReconnectTimer.once(2, connectToWifi);
+}
 
 void setup()
 {
@@ -20,6 +44,11 @@ void setup()
     }
 
     bme.setTemperatureCompensation(-0.9);
+
+    wifiConnectHandler = WiFi.onStationModeGotIP(onWifiConnect);
+    wifiDisconnectHandler = WiFi.onStationModeDisconnected(onWifiDisconnect);
+
+    connectToWifi();
 }
 
 void loop()
