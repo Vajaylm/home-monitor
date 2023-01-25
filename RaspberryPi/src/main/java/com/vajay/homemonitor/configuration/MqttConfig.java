e0bfb0c4bb18c59e14f8cbc6e1fde6ca520c87f5
László Miklós Vajay <vajaylm@gmail.com>
2023-01-25 15:09:06 +0100

    Implement MQTT Client subscription, basic message handling

---
+++ RaspberryPi/src/main/java/com/vajay/homemonitor/configuration/MqttConfig.java
@@ -0,0 +1,65 @@
+package com.vajay.homemonitor.configuration;
+
+import java.util.Map;
+
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.core.MessageProducer;
+import org.springframework.integration.mqtt.core.DefaultMqttPahoClientFactory;
+import org.springframework.integration.mqtt.core.MqttPahoClientFactory;
+import org.springframework.integration.mqtt.inbound.MqttPahoMessageDrivenChannelAdapter;
+import org.springframework.integration.mqtt.support.DefaultPahoMessageConverter;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHandler;
+import org.springframework.messaging.MessagingException;
+
+@Configuration
+public class MqttConfig {
+    private Map<String, String> env = System.getenv();
+
+    @Bean
+    public MessageChannel mqttInputChannel() {
+        return new DirectChannel();
+    }
+
+    @Bean
+    public MqttPahoClientFactory mqttClientFactory() {
+        DefaultMqttPahoClientFactory factory = new DefaultMqttPahoClientFactory();
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setServerURIs(new String[] { env.get("mqttBrokerUrl") });
+        options.setUserName(env.get("mqttUsername"));
+        options.setPassword(env.get("mqttPassword").toCharArray());
+        options.setAutomaticReconnect(true);
+        options.setCleanSession(true);
+
+        factory.setConnectionOptions(options);
+        return factory;
+    }
+
+    @Bean
+    public MessageProducer inbound() {
+        MqttPahoMessageDrivenChannelAdapter adapter = new MqttPahoMessageDrivenChannelAdapter("serverIn",
+                mqttClientFactory(), env.get("mqttTopic"));
+        adapter.setCompletionTimeout(5000);
+        adapter.setConverter(new DefaultPahoMessageConverter());
+        adapter.setQos(1);
+        adapter.setOutputChannel(mqttInputChannel());
+        return adapter;
+    }
+
+    @Bean
+    @ServiceActivator(inputChannel = "mqttInputChannel")
+    public MessageHandler handler() {
+        return new MessageHandler() {
+            @Override
+            public void handleMessage(Message<?> message) throws MessagingException {
+                System.out.println(message.getPayload());
+            }
+        };
+    }
+
+}
